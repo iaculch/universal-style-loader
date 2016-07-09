e5e6b102d8445c6869390ad6a4ed38825f3bb76b
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-07-09 15:01:02 -0700

    fixed bug

--- src/lib/utils/wrapper.js
+++ src/lib/utils/wrapper.js
@@ -6,7 +6,7 @@
 /** Creates a function to wrap string code blocks for better debugging. */
 export default function wrapper (fileName) {
   return function wrap (code, fnName) {
-    const location = `${fileName}${fn ? ` => ${fnName}` : ''}`
+    const location = `${fileName}${fnName ? ` => ${fnName}` : ''}`
     return `/** ${location}: start */
 ${code}
 /** ${location}: end */`
